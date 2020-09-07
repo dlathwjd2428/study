45fc4738199b776571d3419bc2f0e8a4e5b2c3c8
OneStone3647 <onestone3647@gmail.com>
2020-09-07 14:55:08 +0900

    answer

--- C_C++/문제 풀이/Chapter8/Chapter8_문자열 함수/string/main.cpp
+++ C_C++/문제 풀이/Chapter8/Chapter8_문자열 함수/string/main.cpp
@@ -2,65 +2,72 @@
 #include<stdio.h>
 #include<string.h>
 
-char* Strlen(char str[]);
-char* Strcpy(char* Name, char* My_Name);
-char* Strcat(char str[]);
-int Strcmp(char a[], char b[]);
+int Strlen(const char *str);
+char Strcpy(const char *name1, char *name2);
+
+//char* Strcpy(char* Name, char* My_Name);
+//char* Strcat(char str[]);
+//int Strcmp(char a[], char b[]);
 
 void main()
 {
-	char str[10] = "Hello";
-	char Name[10];
-	char My_Name[10];
+	char str[100] = { 0 };
+	printf("문장 입력:");
+	scanf("%s", &str);
+	printf("문자열 길이 : %d\n",Strlen(str));
 
-	char str1[10] = "string!!";
-	char str2[10] = "string";
 
-	printf("이름입력 > ");
-	scanf("%s", &My_Name);
+	char name1[10] = "hello";
+	char name2[10];
 
-	Strlen(str);
-	Strcpy(Name, My_Name);
-	Strcat(str);
-	printf("%s==%s:%d\n", str1, str2, Strcmp(str1, str2));
+	Strcpy(name1, name2);
+
+	printf("name1 : %s name2 : %s", Strcpy(name1, name2));
+
+
 }
 
-char* Strlen(char str[])
+int Strlen(const char *str)
 {
-	printf("%s문자열의 길이 : %d\n", str, strlen(str));
-	return str;
-}
+	const char *string_str = str;
 
-char* Strcpy(char* Name, char* My_Name)
-{
-	strcpy(Name, My_Name);
-	printf("Name : %s\n", Name);
-	printf("My_Name : %s\n", My_Name);
-
-	return Name;
-}
-
-char* Strcat(char str[])
-{
-	printf("%s\n", str);
-	strcat(str, "^^");
-	printf("%s\n", str);
-
-	return str;
-}
-
-int Strcmp(char a[], char b[])
-{
-	int i;
-
-	for (i = 0;; i++)
+	while (*str != 0)
 	{
-		if (a[i] == b[i] && a[i] == NULL)
-			return 0;
-		else if (a[i] > b[i])
-			return 1;
-		else if (a[i] < b[i])
-			return -1;
+		*str++;
 	}
 
+	return str - string_str;
 }
+
+char Strcpy(const char *name1, char *name2)
+{
+	*name2 = *name1;
+	
+	return *name2;
+}
+
+//}
+//
+//char* Strcat(char str[])
+//{
+//	printf("%s\n", str);
+//	strcat(str, "^^");
+//	printf("%s\n", str);
+//
+//	return str;
+//}
+//
+//int Strcmp(char a[], char b[])
+//{
+//	int i;
+//
+//	for (i = 0;; i++)
+//	{
+//		if (a[i] == b[i] && a[i] == NULL)
+//			return 0;
+//		else if (a[i] > b[i])
+//			return 1;
+//		else if (a[i] < b[i])
+//			return -1;
+//	}
+//}
