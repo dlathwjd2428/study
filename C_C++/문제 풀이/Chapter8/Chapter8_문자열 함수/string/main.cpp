320c34fb2e1f6743150eb9df7710d933ec012fd9
OneStone3647 <onestone3647@gmail.com>
2020-09-08 14:08:20 +0900

    answer

--- C_C++/문제 풀이/Chapter8/Chapter8_문자열 함수/string/main.cpp
+++ C_C++/문제 풀이/Chapter8/Chapter8_문자열 함수/string/main.cpp
@@ -4,10 +4,8 @@
 
 int Strlen(const char *str);
 char Strcpy(const char *name1, char *name2);
-
-//char* Strcpy(char* Name, char* My_Name);
-//char* Strcat(char str[]);
-//int Strcmp(char a[], char b[]);
+int Strcmp(char a[], char b[]);
+char Strcat(char *name1, char *s_cat);
 
 void main()
 {
@@ -16,15 +14,35 @@ void main()
 	scanf("%s", &str);
 	printf("문자열 길이 : %d\n",Strlen(str));
 
+	////
 
 	char name1[10] = "hello";
-	char name2[10];
+	char name2[10] = { 0 };
+	char s_cmp[10] = "hello!!";
 
 	Strcpy(name1, name2);
 
-	printf("name1 : %s name2 : %s", Strcpy(name1, name2));
+	printf("name1 : %s name2 : %s\n", name1, name2);
 
+	////
 
+	Strcmp(name1, s_cmp);
+	if (Strcmp(name1, s_cmp) == 0)
+	{
+		printf("%s == %s\n", name1, s_cmp);
+	}
+	else
+	{
+		printf("%s != %s\n", name1, s_cmp);
+	}
+
+	////
+
+	char s_cat[10] = "^^";
+
+	printf("%s\n", name1);
+	Strcat(name1, s_cat);
+	printf("%s", name1);
 }
 
 int Strlen(const char *str)
@@ -41,33 +59,44 @@ int Strlen(const char *str)
 
 char Strcpy(const char *name1, char *name2)
 {
-	*name2 = *name1;
-	
-	return *name2;
+	char *temp = name2;
+
+	while (*name1)
+	{
+		*name2++ = *name1++;
+	}
+
+	return *temp;
 }
 
-//}
-//
-//char* Strcat(char str[])
-//{
-//	printf("%s\n", str);
-//	strcat(str, "^^");
-//	printf("%s\n", str);
-//
-//	return str;
-//}
-//
-//int Strcmp(char a[], char b[])
-//{
-//	int i;
-//
-//	for (i = 0;; i++)
-//	{
-//		if (a[i] == b[i] && a[i] == NULL)
-//			return 0;
-//		else if (a[i] > b[i])
-//			return 1;
-//		else if (a[i] < b[i])
-//			return -1;
-//	}
-//}
+int Strcmp(char a[], char b[])
+{
+	int i;
+
+	for (i = 0;; i++)
+	{
+		if (a[i] == b[i] && a[i] == NULL)
+			return 0;
+		else if (a[i] > b[i])
+			return 1;
+		else if (a[i] < b[i])
+			return -1;
+	}
+}
+
+char Strcat(char *name1, char *s_cat)
+{
+	int i = 0;
+
+	while(name1[i] != '\0')
+	{
+		i++;
+	}
+
+	for (int j = 0; s_cat[j] != '\0'; j++)
+		name1[i + j] = s_cat[j];
+
+	return *name1;
+}
+
+
