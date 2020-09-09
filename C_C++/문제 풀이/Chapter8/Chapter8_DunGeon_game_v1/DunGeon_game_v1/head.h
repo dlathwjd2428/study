849246fa3c9da645f607a7bcced62273ce41905f
OneStone3647 <onestone3647@gmail.com>
2020-09-09 14:57:29 +0900

    answer

---
+++ C_C++/문제 풀이/Chapter8/Chapter8_DunGeon_game_v1/DunGeon_game_v1/head.h
@@ -0,0 +1,31 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <windows.h>
+
+#define MAX 8
+
+enum
+{
+	INDUNGEON = 1,
+	PLAYER_INFO,
+	SAVE,
+	LOAD,
+	EXIT
+};
+
+typedef struct _Character
+{
+	char name[20];
+	int offense; //공격력
+	int defense; //방어력
+	int HP; //현재 체력
+	int Max_HP; //최대 체력
+	float EXP; //현재 경험치
+	float Max_EXP; //최대 경험치	
+	float Die_EXP; //죽었을때 주는 경험치
+	bool defense_on; //방어 on/off
+	int level; //레벨
+	//player와 enemy 구분 변수
+	//enemy일 경우 공격 속도 (10당 0.1초)
+}Character;
---
+++ C_C++/문제 풀이/Chapter8/Chapter8_DunGeon_game_v1/DunGeon_game_v1/main.cpp
@@ -0,0 +1,120 @@
+#include "head.h""
+
+int menu();
+void Player_info(Character* player[]);
+void Default_file(Character* Char_List[]);
+
+int main()
+{
+
+	Character* Char_List[MAX];
+
+	Character new_player;
+	printf("플레이어 이름 입력 : ");
+
+	Default_file(Char_List);
+
+	while (1)
+	{
+		Char_List[MAX] = (Character*)malloc(sizeof(Character));
+		
+		
+		system("cls");
+		switch (menu())
+		{
+		case INDUNGEON:
+
+			break;
+
+		case PLAYER_INFO:
+			Player_info(Char_List);
+			break;
+
+		case SAVE:
+			break;
+		case LOAD:
+			break;
+
+		case EXIT:
+			break;
+
+		default:
+			printf("잘못입력하였습니다. 다시 입력해주세요.");
+			Sleep(700);
+			break;
+		}
+	}
+	
+}
+
+int menu()
+{
+	int ch = 0;
+
+	printf("=======DunGeon=======\n");
+	printf("    1.던전 입장\n");
+	printf("    2.Player 정보\n");
+	printf("    3.Save\n");
+	printf("    4.Load\n");
+	printf("    5.종료\n");
+	printf("    입력> ");
+
+	scanf("%d", &ch);
+
+	return ch;
+}
+
+void Default_file(Character* Char_List[])
+{
+	FILE* Default_fr = fopen("Default.txt", "r");
+
+	if (Default_fr == NULL)
+		printf("가져올 데이터가 없습니다, \n");
+	else
+	{
+		for (int i = 0; i < MAX; i++)
+		{
+			Char_List[i] = (Character*)malloc(sizeof(Character));
+
+			if (i == 0)
+			{
+				scanf("%s", Char_List[0]->name);
+			}
+			else
+			{
+				fscanf(Default_fr, "%s", Char_List[i]->name);
+			}
+			fscanf(Default_fr, "%d %d %d %d %f %f %f %d %d", &Char_List[i]->offense,
+															 &Char_List[i]->defense,
+															 &Char_List[i]->HP,
+															 &Char_List[i]->Max_HP,
+															 &Char_List[i]->EXP,
+															 &Char_List[i]->Max_EXP,
+															 &Char_List[i]->Die_EXP,
+															 &Char_List[i]->defense_on,
+															 &Char_List[i]->level);
+		}
+		fclose(Default_fr);
+	}
+}
+
+//void SaveFile(Character* Student_List[])
+//{
+//	FILE* SaveFile = fopen("StudentFile.txt", "w");
+//
+//	
+//
+//	fclose(SaveFile);
+//
+//	printf("저장이 완료되었습니다. \n");
+//	system("Pause");
+//}
+
+void Player_info(Character* player[])
+{
+	system("cls");
+	printf("======%s<%dLv>======\n", player[0]->name, player[0]->level);
+	printf("공격력 = %d          방어력 = %d       생명력 = %d/50\n", player[0]->offense, player[0]->defense, player[0]->HP);
+	printf("경험치 = %d/10       GetEXP : %d\n", player[0]->EXP, player[0]->EXP);
+	system("pause");
+}
