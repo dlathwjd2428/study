fa36b50e9da4a8efd5bcb1fc2ba68412506ffa87
dlathwjd2428 <69287243+dlathwjd2428@users.noreply.github.com>
2020-09-22 15:01:06 +0900

    answer

--- C_C++/문제 풀이/Chapter8/Chapter8_DunGeon_game_v1/DunGeon_game_v1/head.h
+++ C_C++/문제 풀이/Chapter8/Chapter8_DunGeon_game_v1/DunGeon_game_v1/head.h
@@ -19,13 +19,13 @@ typedef struct _Character
 	char name[20];
 	int offense; //공격력
 	int defense; //방어력
-	int HP; //현재 체력
+	int HP = 50; //현재 체력
 	int Max_HP; //최대 체력
-	float EXP; //현재 경험치
+	float EXP = 0; //현재 경험치
 	float Max_EXP; //최대 경험치	
-	float Die_EXP; //죽었을때 주는 경험치
-	bool defense_on; //방어 on/off
+	float Get_EXP; //죽었을때 주는 경험치
+	int defense_on; //방어 on/off
 	int level; //레벨
-	bool player; //player와 enemy 구분 변수
+	int player; //player와 enemy 구분 변수
 	int attack_speed;//enemy일 경우 공격 속도 (10당 0.1초)
 }Character;
--- C_C++/문제 풀이/Chapter8/Chapter8_DunGeon_game_v1/DunGeon_game_v1/main.cpp
+++ C_C++/문제 풀이/Chapter8/Chapter8_DunGeon_game_v1/DunGeon_game_v1/main.cpp
@@ -3,6 +3,10 @@
 int menu();
 void Player_info(Character* player[]);
 void Default_file(Character* Char_List[]);
+int Dungeon_menu(Character* monster[]);
+int save_menu();
+
+int monster_max = 0;
 
 int main()
 {
@@ -10,7 +14,7 @@ int main()
 	Character* Char_List[MAX];
 
 	Character new_player;
-	printf("플레이어 이름 입력 : ");
+
 
 	Default_file(Char_List);
 
@@ -23,7 +27,7 @@ int main()
 		switch (menu())
 		{
 		case INDUNGEON:
-
+			Dungeon_menu(Char_List);
 			break;
 
 		case PLAYER_INFO:
@@ -31,6 +35,7 @@ int main()
 			break;
 
 		case SAVE:
+			Save_menu();
 			break;
 		case LOAD:
 			break;
@@ -68,25 +73,16 @@ void Default_file(Character* Char_List[])
 {
 	FILE* Default_fr = fopen("Default.txt", "r");
 
+	printf("플레이어 이름 입력 : ");
+
 	if (Default_fr == NULL)
 		printf("가져올 데이터가 없습니다, \n");
 	else
 	{
-		Char_List[0] = (Character*)malloc(sizeof(Character));
-		scanf("%s", Char_List[0]->name);
-		fscanf(Default_fr, "%d %d %d %d %f %f %f %d %d", &Char_List[0]->offense,
-														&Char_List[0]->defense,
-														&Char_List[0]->Max_HP,
-														&Char_List[0]->Max_EXP,
-														&Char_List[0]->Die_EXP,
-														&Char_List[0]->level,
-														&Char_List[0]->player,
-														&Char_List[0]->attack_speed);
-
-		for (int i = 0; i < MAX; i++)
+		fscanf(Default_fr, "%d", &monster_max);
+		for (int i = 0; i <= monster_max; i++)
 		{
 			Char_List[i] = (Character*)malloc(sizeof(Character));
-
 			if (i == 0)
 			{
 				scanf("%s", Char_List[0]->name);
@@ -95,36 +91,136 @@ void Default_file(Character* Char_List[])
 			{
 				fscanf(Default_fr, "%s", Char_List[i]->name);
 			}
-			fscanf(Default_fr, "%d %d %d %d %f %f %f %d %d", &Char_List[i]->offense,
-															 &Char_List[i]->defense,
-															 &Char_List[i]->Max_HP,
-															 &Char_List[i]->Max_EXP,
-															 &Char_List[i]->Die_EXP,
-															 &Char_List[i]->level,
-															 &Char_List[i]->player,
-															 &Char_List[i]->attack_speed);
+			fscanf(Default_fr, "%d %d %d %f %f %d %d %d", &Char_List[i]->offense,
+														  &Char_List[i]->defense,
+														  &Char_List[i]->Max_HP,
+														  &Char_List[i]->Max_EXP,
+														  &Char_List[i]->Get_EXP,
+														  &Char_List[i]->level,
+														  &Char_List[i]->player,
+														  &Char_List[i]->attack_speed);
 		}
 		fclose(Default_fr);
 	}
 }
 
-//void SaveFile(Character* Student_List[])
-//{
-//	FILE* SaveFile = fopen("StudentFile.txt", "w");
-//
-//	
-//
-//	fclose(SaveFile);
-//
-//	printf("저장이 완료되었습니다. \n");
-//	system("Pause");
-//}
+int Save_menu()
+{
+	int ch = 0;
+
+	printf("=====Save=====\n");
+	for (int i = 1; i <= 10; i++)
+	{
+		printf("%d번슬롯 : <파일여부 : ", i);
+	}
+	printf("선택 : ");
+
+	scanf("%d", &ch);
+
+	return ch;
+}
+
+void SaveFile_r(Character* Char_List[])
+{
+	FILE* Save_fr = fopen("SaveFile1.txt", "r");
+
+	if (Save_fr == NULL)
+		printf("가져올 데이터가 없습니다, \n");
+	else
+	{
+		for (int i = 0; i < MAX ; i++)
+		{
+			if (i == 1)
+			{
+				fscanf(Save_fr, "%d", &monster_max);
+			}
+			Char_List[i] = (Character*)malloc(sizeof(Character));
+			fscanf(Save_fr, "%s %d %d %d %d %d %f %f %d %f %d %d", &Char_List[i]->name,
+																	&Char_List[i]->offense,
+																	&Char_List[i]->attack_speed,
+																	&Char_List[i]->HP,
+																	&Char_List[i]->defense,
+																	&Char_List[i]->defense_on,
+																	&Char_List[i]->EXP,
+																	&Char_List[i]->Get_EXP,
+																	&Char_List[i]->level,
+																	&Char_List[i]->Max_EXP,
+																	&Char_List[i]->Max_HP,
+																	&Char_List[i]->player);		
+		}
+		fclose(Save_fr);		
+	}
+}
+
+void SaveFile_w(Character* Char_List[])
+{
+	FILE* Save_fw = fopen("SaveFile1.txt", "w");
+
+	for (int i = 0; i < MAX; i++)
+	{
+		if (i == 1)
+		{
+			fprintf(Save_fw, "%d", &monster_max);
+		}
+		fprintf(Save_fw, "%s %d %d %d %d %d %f %f %d %f %d %d", &Char_List[i]->name,
+																&Char_List[i]->offense,
+																&Char_List[i]->attack_speed,
+																&Char_List[i]->HP,
+																&Char_List[i]->defense,
+																&Char_List[i]->defense_on,
+																&Char_List[i]->EXP,
+																&Char_List[i]->Get_EXP,
+																&Char_List[i]->level,
+																&Char_List[i]->Max_EXP,
+																&Char_List[i]->Max_HP,
+																&Char_List[i]->player);
+	}
+		
+	fclose(Save_fw);
+
+	printf("저장이 완료되었습니다. \n");
+	system("Pause");
+}
 
 void Player_info(Character* player[])
 {
 	system("cls");
 	printf("======%s<%dLv>======\n", player[0]->name, player[0]->level);
-	printf("공격력 = %d          방어력 = %d       생명력 = %d/50\n", player[0]->offense, player[0]->defense, player[0]->HP);
-	printf("경험치 = %d/10       GetEXP : %d\n", player[0]->EXP, player[0]->EXP);
+	printf("공격력 = %d          방어력 = %d       생명력 = %d/50\n", player[0]->offense, player[0]->defense, player[0]->Max_HP);
+	printf("경험치 = %d/10       GetEXP : %d\n", player[0]->Max_EXP, player[0]->Get_EXP);
 	system("pause");
+}
+
+int Dungeon_menu(Character* monster[])
+{
+	int ch = 0;
+
+	system("cls");
+	printf("=====던전 입구=====\n");
+	for (int i = 1; i < MAX; i++)
+	{
+		printf("  %d.%d층던전 : [%s]\n", i, i, monster[i]->name);
+	}
+	printf("  8.돌아가기\n");
+	printf("선택 : ");
+	scanf("%d", &ch);
+
+	return ch;
+}
+
+void attack(Character* player[])
+{
+	printf("조작 키 : Space<공격>,z<방어>\n");
+	printf("======%s<%dLv>======\n", player[0]->name, player[0]->level);
+	printf("공격력 = %d          방어력 = %d       생명력 = %d/50\n", player[0]->offense, player[0]->defense, player[0]->Max_HP);
+	printf("경험치 = %d/10       GetEXP : %d\n", player[0]->Max_EXP, player[0]->Get_EXP);
+	printf("방어모드 : On\n");
+	printf("================\n");
+	printf("======%s<%dLv>======\n", player[1]->name, player[1]->level);
+	printf("공격력 = %d          방어력 = %d       생명력 = %d/50\n", player[0]->offense, player[0]->defense, player[0]->Max_HP);
+	printf("경험치 = %d/10       GetEXP : %d\n", player[0]->Max_EXP, player[0]->Get_EXP);
+	printf("방어모드 : On\n");
+	printf("================\n");
+
+
 }
