be83f431d0230dc1c7daf45e7d5a91c036285b25
dlathwjd2428 <69287243+dlathwjd2428@users.noreply.github.com>
2020-09-23 14:57:23 +0900

    answer

--- C_C++/문제 풀이/Chapter8/Chapter8_DunGeon_game_v1/DunGeon_game_v1/main.cpp
+++ C_C++/문제 풀이/Chapter8/Chapter8_DunGeon_game_v1/DunGeon_game_v1/main.cpp
@@ -4,17 +4,14 @@ int menu();
 void Player_info(Character* player[]);
 void Default_file(Character* Char_List[]);
 int Dungeon_menu(Character* monster[]);
-int save_menu();
+void SaveFile_w(Character* Char_List[]);
+void SaveFile_r(Character* Char_List[]);
 
 int monster_max = 0;
 
 int main()
 {
-
 	Character* Char_List[MAX];
-
-	Character new_player;
-
 
 	Default_file(Char_List);
 
@@ -35,9 +32,10 @@ int main()
 			break;
 
 		case SAVE:
-			Save_menu();
+			SaveFile_w(Char_List);
 			break;
 		case LOAD:
+			SaveFile_r(Char_List);
 			break;
 
 		case EXIT:
@@ -71,16 +69,15 @@ int menu()
 
 void Default_file(Character* Char_List[])
 {
-	FILE* Default_fr = fopen("Default.txt", "r");
-
 	printf("플레이어 이름 입력 : ");
+
+	FILE* Default_fr = fopen("Default.txt", "r");
 
 	if (Default_fr == NULL)
 		printf("가져올 데이터가 없습니다, \n");
 	else
 	{
-		fscanf(Default_fr, "%d", &monster_max);
-		for (int i = 0; i <= monster_max; i++)
+		for (int i = 0; i <= MAX; i++)
 		{
 			Char_List[i] = (Character*)malloc(sizeof(Character));
 			if (i == 0)
@@ -92,23 +89,28 @@ void Default_file(Character* Char_List[])
 				fscanf(Default_fr, "%s", Char_List[i]->name);
 			}
 			fscanf(Default_fr, "%d %d %d %f %f %d %d %d", &Char_List[i]->offense,
-														  &Char_List[i]->defense,
-														  &Char_List[i]->Max_HP,
-														  &Char_List[i]->Max_EXP,
-														  &Char_List[i]->Get_EXP,
-														  &Char_List[i]->level,
-														  &Char_List[i]->player,
-														  &Char_List[i]->attack_speed);
+															&Char_List[i]->defense,
+															&Char_List[i]->Max_HP,
+															&Char_List[i]->Max_EXP,
+															&Char_List[i]->Get_EXP,
+															&Char_List[i]->level,
+															&Char_List[i]->player,
+															&Char_List[i]->attack_speed);
+
+			fscanf(Default_fr, "%d", &monster_max);
 		}
 		fclose(Default_fr);
 	}
 }
 
-int Save_menu()
+void SaveFile_r(Character* Char_List[])
 {
 	int ch = 0;
+	char file_num[10];
 
-	printf("=====Save=====\n");
+	FILE* Save_fr;
+
+	printf("=====Load=====\n");
 	for (int i = 1; i <= 10; i++)
 	{
 		printf("%d번슬롯 : <파일여부 : ", i);
@@ -117,39 +119,40 @@ int Save_menu()
 
 	scanf("%d", &ch);
 
-	return ch;
-}
-
-void SaveFile_r(Character* Char_List[])
-{
-	FILE* Save_fr = fopen("SaveFile1.txt", "r");
-
-	if (Save_fr == NULL)
-		printf("가져올 데이터가 없습니다, \n");
-	else
+	for (int num = '1'; num <= '10'; num++)
 	{
-		for (int i = 0; i < MAX ; i++)
+		sprintf(file_num, "SaveFile%c.txt", num);
+		Save_fr = fopen(file_num, "r");
+
+		if (Save_fr == NULL)
+			printf("가져올 데이터가 없습니다, \n");
+		else
 		{
-			if (i == 1)
+			for (int i = 0; i < MAX; i++)
 			{
-				fscanf(Save_fr, "%d", &monster_max);
+				if (i == 1)
+				{
+					fscanf(Save_fr, "%d", &monster_max);
+				}
+				Char_List[i] = (Character*)malloc(sizeof(Character));
+				fscanf(Save_fr, "%s %d %d %d %d %d %f %f %d %f %d %d", &Char_List[i]->name,
+					&Char_List[i]->offense,
+					&Char_List[i]->attack_speed,
+					&Char_List[i]->HP,
+					&Char_List[i]->defense,
+					&Char_List[i]->defense_on,
+					&Char_List[i]->EXP,
+					&Char_List[i]->Get_EXP,
+					&Char_List[i]->level,
+					&Char_List[i]->Max_EXP,
+					&Char_List[i]->Max_HP,
+					&Char_List[i]->player);
 			}
-			Char_List[i] = (Character*)malloc(sizeof(Character));
-			fscanf(Save_fr, "%s %d %d %d %d %d %f %f %d %f %d %d", &Char_List[i]->name,
-																	&Char_List[i]->offense,
-																	&Char_List[i]->attack_speed,
-																	&Char_List[i]->HP,
-																	&Char_List[i]->defense,
-																	&Char_List[i]->defense_on,
-																	&Char_List[i]->EXP,
-																	&Char_List[i]->Get_EXP,
-																	&Char_List[i]->level,
-																	&Char_List[i]->Max_EXP,
-																	&Char_List[i]->Max_HP,
-																	&Char_List[i]->player);		
+			fclose(Save_fr);
 		}
-		fclose(Save_fr);		
 	}
+
+	
 }
 
 void SaveFile_w(Character* Char_List[])
@@ -164,14 +167,26 @@ void SaveFile_w(Character* Char_List[])
 		fclose(fp);
 	}*/
 
-	FILE* Save_fw = fopen("SaveFile1.txt", "w");
+	system("cls");
+
+	char ch = 0;
+
+	printf("=====Save=====");
+	for (int i = 1; i <= 10; i++)
+	{
+		printf("\n%d번슬롯 : <파일여부 : ", i);
+	}
+	printf("\n선택 : ");
+
+	scanf("%c", &ch);
+
+	FILE* Save_fw;
+	char file_num[20];
+	sprintf(file_num, "SaveFile%d.txt", ch-48);
+	Save_fw = fopen(file_num, "w");
 
 	for (int i = 0; i < MAX; i++)
 	{
-		if (i == 1)
-		{
-			fprintf(Save_fw, "%d", &monster_max);
-		}
 		fprintf(Save_fw, "%s %d %d %d %d %d %f %f %d %f %d %d", &Char_List[i]->name,
 																&Char_List[i]->offense,
 																&Char_List[i]->attack_speed,
@@ -231,6 +246,4 @@ void attack(Character* player[])
 	printf("경험치 = %d/10       GetEXP : %d\n", player[0]->Max_EXP, player[0]->Get_EXP);
 	printf("방어모드 : On\n");
 	printf("================\n");
-
-
 }
