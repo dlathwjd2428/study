b299b121e15914944722897a99fdaa0dca3af364
dlathwjd2428 <69287243+dlathwjd2428@users.noreply.github.com>
2020-11-02 14:58:02 +0900

    answer

---
+++ C_C++/문제 풀이/C++/Chapter 3/chapter3_학생관리 프로그램/Student/Student.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Student
+{
+private:
+	string m_strName;
+	int m_iAge;
+	bool b_Gender;
+	CLASS m_eClass;
+	int m_iNumber;
+public:
+
+};
