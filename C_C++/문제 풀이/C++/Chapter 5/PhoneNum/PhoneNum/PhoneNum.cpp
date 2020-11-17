7619d12becd1302a337048d89e18afb493d24485
dlathwjd2428 <69287243+dlathwjd2428@users.noreply.github.com>
2020-11-17 14:15:30 +0900

    answer

--- C_C++/문제 풀이/C++/Chapter 5/PhoneNum/PhoneNum/PhoneNum.cpp
+++ C_C++/문제 풀이/C++/Chapter 5/PhoneNum/PhoneNum/PhoneNum.cpp
@@ -7,32 +7,30 @@ Phone::Phone()
 	printf("전화번호 입력 : ");
 	cin >> GetNum;
 
-	PhoneNum = new string[GetNum.length() + 1];
-	strcpy(PhoneNum, GetNum.c_str());
-
+	PhoneNum = new string(GetNum);
 }
 
 void Phone::PrintPhoneNum()
 {
 	cout << "완성된 번호 : ";
 
-	if (PhoneNum.length() == 10)
+	if (PhoneNum->length() == 10)
 	{
-		cout << PhoneNum[0] << PhoneNum[1] << PhoneNum[2] << "-"
-			<< PhoneNum[3] << PhoneNum[4] << PhoneNum[5] << "-"
-			<< PhoneNum[6] << PhoneNum[7] << PhoneNum[8] << PhoneNum[9];
+		cout << PhoneNum->at(0) << PhoneNum->at(1) << PhoneNum->at(2) << "-"
+			<< PhoneNum->at(3) << PhoneNum->at(4) << PhoneNum->at(5) << "-"
+			<< PhoneNum->at(6) << PhoneNum->at(7) << PhoneNum->at(8) << PhoneNum->at(9)<<endl;
 	}
-	else if (PhoneNum.length() == 11)
+	else if (PhoneNum->length() == 11)
 	{
-		cout << PhoneNum[0] << PhoneNum[1] << PhoneNum[2] << "-"
-			<< PhoneNum[3] << PhoneNum[4] << PhoneNum[5] << PhoneNum[6] << "-"
-			<< PhoneNum[7] << PhoneNum[8] << PhoneNum[9] << PhoneNum[10];
+		cout << PhoneNum->at(0) << PhoneNum->at(1) << PhoneNum->at(2) << "-"
+			<< PhoneNum->at(3) << PhoneNum->at(4) << PhoneNum->at(5) << PhoneNum->at(6) << "-"
+			<< PhoneNum->at(7) << PhoneNum->at(8) << PhoneNum->at(9) << PhoneNum->at(10)<<endl;
 	}
 }
 
 void Phone::Checklength()
 {
-	if (PhoneNum.length() < 10 || PhoneNum.length() > 11)
+	if (PhoneNum->length() < 10 || PhoneNum->length() > 11)
 	{
 		cout << "번호를 잘못 입력하셨습니다." << endl;
 	}
@@ -41,15 +39,15 @@ void Phone::Checklength()
 }
 void Phone::CheckNum()
 {
-	if (PhoneNum[1] == '1') //휴대폰
+	if (PhoneNum->at(1) == '1') //휴대폰
 	{
 		PrintPhoneNum();
 	}
-	else if (PhoneNum[1] == '2') //서울
+	else if (PhoneNum->at(1) == '2') //서울
 	{
 		PrintPhoneNum();
 	}
-	else if (PhoneNum[1] >= '3' && PhoneNum[1] <= '6') //지역
+	else if (PhoneNum->at(1) >= '3' && PhoneNum->at(1) <= '6') //지역
 	{
 		PrintPhoneNum();
 	}
@@ -61,6 +59,6 @@ void Phone::CheckNum()
 
 Phone::~Phone()
 {
-	cout << PhoneNum << " 의 동적 할당을 해제합니다.\n";
-	//delete []PhoneNum;
+	cout << *PhoneNum << " 의 동적 할당을 해제합니다.\n";
+	delete PhoneNum;
 }
--- C_C++/문제 풀이/C++/Chapter 5/PhoneNum/PhoneNum/main.cpp
+++ C_C++/문제 풀이/C++/Chapter 5/PhoneNum/PhoneNum/main.cpp
@@ -2,9 +2,6 @@
 
 void main()
 {
-	while (1)
-	{
-		Phone p1;
-		p1.Checklength();
-	}
+	Phone p1;
+	p1.Checklength();
 }
